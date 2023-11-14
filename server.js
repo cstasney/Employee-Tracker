9aba9d7e62a679a4a6a6919d85cb4fb1699ac30d
cstasney <cstasney07@gmail.com>
2023-11-14 16:16:42 -0500

    dependencies and ANSI font with chalk on app start

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 // dependencies
 const inquirer = require('inquirer')
 const express = require('express')
-const sequelize = require('./conifg/connection') 
+const sequelize = require('./config/connection');
+const figlet = require("figlet");
+const chalk = require('chalk');
+
+console.log(chalk.blue(figlet.textSync("Employee Tracker")));
+console.log(`                                                          ` + chalk.greenBright.bold('Created By: Chris Stasney'));
+
+
+const startApp = () => {
+    inquirer.createPromptModule([])
+}
+
+
+startApp();
+
