f4061e46290710cd83f97d29b9cd24eb05b7f5f8
cstasney <cstasney07@gmail.com>
2023-11-14 13:41:01 -0500

    .env ignore saved

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
 INSERT INTO department(department_name)
-VALUES ("Service"), ("Parts"), ("Sales and Marketing"), ("Finance"), ("Customer Service"), ("HR")
+VALUES ("Service"), ("Parts"), ("Sales and Marketing"), ("Finance"), ("Customer Service"), ("HR");
+
+INSERT INTO role(title, salary, department_id)
+VALUES("Service Advisor", 50000, 1), () 
